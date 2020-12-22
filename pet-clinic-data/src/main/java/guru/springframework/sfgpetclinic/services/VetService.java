9885f9f5b822fa55b655d57c95e3c844d90c552d
amritoV <verrelliamrito94@gmail.com>
2020-12-22 15:46:31 +0100

    Added List of Vets on vet-index page. CLose #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 
 public interface VetService extends CrudService<Vet,Long> {
 
