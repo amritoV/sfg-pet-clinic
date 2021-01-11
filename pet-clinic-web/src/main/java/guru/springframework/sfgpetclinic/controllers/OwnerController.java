05ee1593e4e79b1ddda523d82d932ad22a690edd
amritoV <verrelliamrito94@gmail.com>
2021-01-11 14:48:43 +0100

    Added info on fuctionalies in under costruction-to broken links. close #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -27,5 +27,10 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/find")
+    public String findOwners(){
+         return "notimplemented";
+     }
+
 
 }
