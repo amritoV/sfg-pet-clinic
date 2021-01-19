2ac8ebe53cf48f39cc32fb070d5d000e67d25efc
amritoV <verrelliamrito94@gmail.com>
2021-01-19 11:57:25 +0100

    Aggiunto la classe OwnerSDjpaService.  Close #40

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDjpaService.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OwnerSDjpaService implements OwnerService {
+
+    OwnerRepository ownerRepository;
+
+    public OwnerSDjpaService( OwnerRepository ownerRepository){
+        this.ownerRepository=ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName).orElse(null);
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners=new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
